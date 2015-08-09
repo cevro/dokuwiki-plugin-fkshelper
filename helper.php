14eaf9369d51a2779b1bab4e8c5384c6c64971bd
Mišo <miso@fykos.cz>
2015-08-09 16:55:36 +0200

    add datalist

--- helper.php
+++ helper.php
@@ -48,7 +48,7 @@ class helper_plugin_fkshelper extends DokuWiki_Plugin {
      * @param int $lvl
      * 
      */
-    public static function returnmsg($text,$lvl) {
+    public static function ReturnMsg($text,$lvl) {
 
 
         ob_start();
@@ -215,13 +215,13 @@ class helper_plugin_fkshelper extends DokuWiki_Plugin {
      * @param bool $subdir
      * @return array
      */
-    public static function filefromdir($dir,$subdir = true) {
+    public static function FileFromDir($dir,$subdir = true) {
         if(!file_exists($dir)){
             msg('Failed to open stream',-1,'','',MSG_USERS_ONLY);
             //throw new InvalidArgumentException('Directory don\'t exist');
             return;
         }
-        if (is_file($dir)){
+        if(is_file($dir)){
             return array($dir);
         }
 
@@ -339,3 +339,78 @@ function FKS_html_make_tag($tag,$attr = array()) {
 
     return '<'.$tag.' '.buildAttributes($attr).'/>';
 }
+
+
+if(!function_exists('form_makeDatalist')){
+
+    /**
+     * form_makeDatalist
+     * Creates hidden datalist of values 
+     * Attribute list=$id has to be used to join datalist with textfield
+     * The list of values is arrays of (value,text),
+     * or an associative array with the values as keys and text as values.
+     * 
+     * @var $id string id of datalist to contact with textfield
+     * @author  Michal Cervenak <miso@fykos.cz>
+     */
+
+    function form_makeDatalist($id,$values,$attrs = array()) {
+        $options = array();
+        reset($values);
+
+        // for ascociative array
+        if(is_string(key($values))){
+            foreach ($values as $val => $text) {
+                $options[] = array($val,$text);
+            }
+        }else{
+            foreach ($values as $val) {
+
+                if(is_array($val)){
+                    @list($val,$text) = $val;
+                }else{
+                    $text = $val;
+                }
+                $options[] = array($val,$text);
+            }
+        }
+        $elem = array('_elem' => 'datalist','_options' => $options,
+            'id' => $id);
+        return array_merge($elem,$attrs);
+    }
+
+}
+
+if(!function_exists('form_datalist')){
+
+    /**
+     * form_datalist
+     * 
+     * Print the HTML datalist.
+     * @var $attrs array ivt _optonst and id 
+     * 
+     * _options: array of  (value,text) for datalist
+     * @author  Michal Cervenak <miso@fykos.cz>
+     */
+    function form_datalist($attrs) {
+
+        $s = '<datalist '.buildAttributes($attrs,true).'>'.DOKU_LF;
+        if(!empty($attrs['_options'])){
+            foreach ($attrs['_options'] as $opt) {
+                @list($value,$text) = $opt;
+                $p = '';
+                if(is_null($text)){
+                    $text = $value;
+                }
+                $p .= ' value="'.formText($value).'"';
+                $s .= '<option'.$p.'>'.formText($text).'</option>';
+            }
+        }else{
+            $s .= '';
+        }
+        $s .= DOKU_LF.'</datalist>';
+
+        return $s;
+    }
+}
+    
